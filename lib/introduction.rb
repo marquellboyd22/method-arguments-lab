6baa1ecdd1cb7f26e6b5726b19d874c7898c0a5a
Marquell boyd <marquellboyd@icloud.com>
2020-10-03 22:55:58 -0400

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
-def introduction_with_language(phrase)
-   puts  phrase = Josh
-    puts "Hi, my name is #{phrase}."
-    
-  end
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
